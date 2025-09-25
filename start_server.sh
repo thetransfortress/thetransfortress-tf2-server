cbef184a1e0a234db6dc20870bae58c29cc84130
Jeliciousz <jeliciousz@proton.me>
2025-09-25 02:10:31 -0400

    oops thats a token leak

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec ./srcds_run -console -game tf +sv_pure 1 +map koth_product_final +maxplayers 24 +sv_setsteamaccount 6CC49A5EC1DFFBAADBC0522A974A9F3D -autoupdate -steam_dir /home/jeli/steamcmd_linux/ -steamcmd_script /home/jeli/tf2-ds/update.txt
+exec ./srcds_run -console -game tf +sv_pure 1 +map koth_product_final +maxplayers 24 +sv_setsteamaccount $GAMESERVERLOGINTOKEN -autoupdate -steam_dir /home/$USER/steamcmd/ -steamcmd_script ./update-script.txt
