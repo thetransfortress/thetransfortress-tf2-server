cd35d7cc3f072e3f5eed92224a7f7b32f7c77f5d
Jeliciousz <jeliciousz@proton.me>
2025-09-25 01:25:34 -0400

    update install script

--- install-tf2-server.sh
+++ install-tf2-server.sh
@@ -4,16 +4,16 @@
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install libc6:i386 libncurses6:i386 libstdc++6:i386 zlib1g:i386 libbz2-1.0:i386 libcurl3t64-gnutls:i386 libsdl2-2.0-0:i386 xorg
+sudo apt -y install libc6:i386 libncurses6:i386 libstdc++6:i386 zlib1g:i386 libbz2-1.0:i386 libcurl3t64-gnutls:i386 libsdl2-2.0-0:i386
 
 # Install Steam
-sudo apt install steam-installer
-steam-installer
-nohup steam &
+sudo apt -y install steam-installer
+steam
+# nohup steam &
 
-# Instal SteamCMD
-mkdir ~/steamcmd
-cd ~/steamcmd
-wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-tar zxf steamcmd_linux.tar.gz
-./steamcmd.sh +force_install_dir /home/$USER/tf2-ds +login anonymous +app_update 232250 +quit
+# # Instal SteamCMD
+# mkdir ~/steamcmd
+# cd ~/steamcmd
+# wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+# tar zxf steamcmd_linux.tar.gz
+# ./steamcmd.sh +force_install_dir /home/$USER/tf2-ds +login anonymous +app_update 232250 +quit
