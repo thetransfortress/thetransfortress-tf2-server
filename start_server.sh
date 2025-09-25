3e9c00a7120aa44258bd5bfa8e39f6ed60272262
Jeliciousz <jeliciousz@proton.me>
2025-09-25 02:21:00 -0400

    Update start server script

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec ./srcds_run -console -game tf +sv_pure 1 +map koth_product_final +maxplayers 24 +sv_setsteamaccount $GAMESERVERLOGINTOKEN -autoupdate -steam_dir /home/$USER/steamcmd/ -steamcmd_script ./update-script.txt
+exec ./srcds_run -console -game tf +sv_pure 1 +map koth_product_final +maxplayers 24 +sv_setsteamaccount $GAMESERVERLOGINTOKEN -autoupdate -steam_dir /home/$USER/steamcmd/ -steamcmd_script ./update-server-script.txt
