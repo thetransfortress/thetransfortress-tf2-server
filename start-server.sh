e742dd433829c9a941c3d9d8195a807b09629bef
Jeliciousz <jeliciousz@proton.me>
2026-05-28 22:22:03 -0400

    use environment variables for ports

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec ./srcds_run -console -game tf -port 27015 -tv_port 27020 +sv_pure 1 +map koth_product_final +maxplayers 24 -autoupdate -steam_dir /home/$USER/.steam/steamcmd/ -steamcmd_script /home/$USER/thetransfortress-tf2-server/update-server-script.txt +sv_setsteamaccount $GAMESERVERLOGINTOKEN
+exec ./srcds_run -console -game tf -port $SRCDS_PORT -tv_port $SRCDS_TV_PORT +sv_pure 1 +map koth_product_final +maxplayers 24 -autoupdate -steam_dir /home/$USER/.steam/steamcmd/ -steamcmd_script /home/$USER/thetransfortress-tf2-server/update-server-script.txt +sv_setsteamaccount $GAMESERVERLOGINTOKEN
